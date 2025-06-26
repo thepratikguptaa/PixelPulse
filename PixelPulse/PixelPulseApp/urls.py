a198bff63f633f64463316925bf3991593246ab2
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-26 14:41:56 +0530

    integrated a post list template

--- PixelPulse/PixelPulseApp/urls.py
+++ PixelPulse/PixelPulseApp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.post_list, name='post_list'),
+    path('create/', views.post_create, name='post_create'),
+    path('<int:post_id>/edit/', views.post_edit, name='post_edit'),
+    path('<int:post_id>/delete/', views.post_delete, name='post_delete'),
+
 ]
